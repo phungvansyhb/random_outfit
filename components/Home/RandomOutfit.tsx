5d100b5f656f635704af06e56b67a3a223635047
michel_phung <phungvansyhb@gmail.com>
2024-09-18 22:18:24 +0700

    fix text

--- components/Home/RandomOutfit.tsx
+++ components/Home/RandomOutfit.tsx
@@ -3,13 +3,14 @@ import { clothesType } from '@/db/schema';
 import { CLOTH_TYPE } from '@/utils/constant';
 import { Card, CardHeader, CardBody, Image, Button } from '@nextui-org/react';
 import { DicesIcon } from 'lucide-react';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 type Props = {
 	listCloth: clothesType[];
 };
 export default function RandomOutfit({ listCloth }: Props) {
 	const IMAGESIZE = 200;
+	const [count, setCount] = useState(0);
 	const randomItem = useCallback(
 		(type: string) => {
 			const list = listCloth.filter((item) => item.type === type);
@@ -17,22 +18,25 @@ export default function RandomOutfit({ listCloth }: Props) {
 				return list[Math.floor(Math.random() * list.length)];
 			} else return null;
 		},
-		[listCloth]
+		[listCloth, count]
 	);
 	return (
 		<Card className='py-4 w-full lg:w-1/3'>
 			<CardHeader className='pb-0 pt-2 px-4 flex justify-between items-start'>
 				<div>
-					<p className='text-tiny uppercase font-bold'>Daily Mix</p>
-					<small className='text-default-500'>12 Clothes</small>
-					<h4 className='font-bold text-large'>Suggest outfit</h4>
+					<p className='text-tiny uppercase font-bold'>Tủ đồ</p>
+					<small className='text-default-500'>
+						<b>{listCloth.length}</b> cái quần cái áo
+					</small>
+					<h4 className='font-bold text-large'>Gợi ý outfit</h4>
 				</div>
 
 				<Button
 					variant='solid'
-					color='primary'>
+					color='primary'
+					onClick={() => setCount(count + 1)}>
 					<DicesIcon />
-					Mix outfit
+					Không thích đâuuu
 				</Button>
 			</CardHeader>
 			<CardBody className='overflow-visible py-2 space-y-4'>
